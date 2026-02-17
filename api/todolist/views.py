39a58f1cf20f83efd940cff4b7f5cd47827463f5
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2026-02-17 15:18:14 +0200

    Update views.py

--- api/todolist/views.py
+++ api/todolist/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
+from .models import Todo
+from .serializers import ToDoItemSerializer
 
-# Create your views here.
+
+class TodoListCreateView(generics.ListCreateAPIView):
+    serializer_class = ToDoItemSerializer
+    permission_classes = [IsAuthenticated]
+
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = ['completed']
+    search_fields = ['title']
+    ordering_fields = ['created_at', 'updated_at']
+
+    def get_queryset(self):
+        return Todo.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class TodoRetrieveDestroyView(generics.RetrieveDestroyAPIView):
+    serializer_class = ToDoItemSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Todo.objects.filter(user=self.request.user)
