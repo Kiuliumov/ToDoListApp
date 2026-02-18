5238eaaed9ba97bc377d3fc78936b109895ae18b
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2026-02-18 23:03:53 +0200

    Update views.py

--- api/accounts/views.py
+++ api/accounts/views.py
@@ -1,7 +1,13 @@
 from rest_framework import generics
-from .serializers import RegisterSerializer
+from .serializers import RegisterSerializer, ProfileSerializer
 from rest_framework.permissions import AllowAny
 
 class RegisterView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
     permission_classes = [AllowAny]
+
+class MeView(generics.RetrieveAPIView):
+    serializer_class = ProfileSerializer
+
+    def get_object(self):
+        return self.request.user
