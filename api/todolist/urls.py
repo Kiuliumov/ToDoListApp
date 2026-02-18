b6204df25ca67f36faa1795932ded3b3b4fa64c5
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2026-02-18 12:23:19 +0200

    Update urls.py

--- api/todolist/urls.py
+++ api/todolist/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import TodoListCreateView, TodoRetrieveDestroyView
 
 urlpatterns = [
-    path('', TodoListCreateView.as_view(), name='todo-list'),
+    path('todos/', TodoListCreateView.as_view(), name='todo-list'),
     path('todos/<int:pk>/', TodoRetrieveDestroyView.as_view(), name='todo-detail'),
 ]
