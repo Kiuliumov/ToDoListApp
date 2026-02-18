91b14493c29485c3e2b92dd09bc1a6908d1d592a
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2026-02-18 12:22:14 +0200

    Update urls.py

--- api/API/urls.py
+++ api/API/urls.py
@@ -14,9 +14,33 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib import admin
 from django.urls import path, include
+from django.contrib import admin
+from django.urls import path, re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="ToDoList API",
+      default_version='v1',
+      description="Interactive API documentation",
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+
+
+SWAGGER_URLS = [
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+]
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('auth/', include('accounts.urls')),
-    path('api/', include('todolist.urls'))
-]
+    path('api/', include('todolist.urls')),
+] + SWAGGER_URLS
